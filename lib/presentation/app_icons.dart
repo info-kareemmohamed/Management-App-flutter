89f77810227754e5a61e67a84ac0380b2f9dfab4
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-10-18 17:22:33 +0300

    app icons

---
+++ lib/presentation/app_icons.dart
@@ -0,0 +1,5 @@
+abstract class AppIcons {
+  static const iconDone = "assets/svgs/done.svg";
+  static const iconNotDone = "assets/svgs/not_done.svg";
+  static const iconClose = "assets/svgs/close.svg";
+}
