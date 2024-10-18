188d6330c9160305764c1fb77f5184de4df34beb
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-10-18 17:33:46 +0300

    constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+abstract class Constants {
+  static const FIREBASE_USERS = "users";
+  static const FIREBASE_TASK = "task";
+  static const FIREBASE_LAST_UPDATED_DATE = "lastUpdatedDate";
+  static const LAST_UPDATED_DATE_KEY = "last_updated_date_key";
+  static const USER_ID = "1";
+}
