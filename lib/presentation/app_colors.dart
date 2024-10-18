ed66a87b478467d042f58f37cf0b25d0129df7ac
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-10-18 17:20:44 +0300

    app colors

---
+++ lib/presentation/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+abstract class AppColors{
+  static const green = Color(0xFF00C95C);
+  static const black = Color(0xFF000000);
+  static const white = Color(0xFFFFFFFF);
+  static const grey = Color(0xFFD9D9D9);
+  static const cardColor = Color(0xFFFCFCFC);//D9D9D9
+  static const lightGreen = Color(0x3F4ECB71); //lightGreen
+}
